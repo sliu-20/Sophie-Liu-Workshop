7e6b3f35258ea05252917986204567686eeb7bd1
Sophie Liu <90879496+sliu-20@users.noreply.github.com>
2021-10-14 23:50:10 -0400

    K14 work

---
+++ 14_form/app.py
@@ -0,0 +1,52 @@
+# Red Wings -- Cameron Nelson, Sophie Liu, Daniel Sooknanan
+# SoftDev
+# K14 - Form and Function
+# 2021-10-14 
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/",methods=['GET', 'POST'])
+def disp_loginpage():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth",methods=['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    if request.method == 'GET':
+        return render_template("response.html",username=request.args['username'],method=request.method);
+    if request.method == 'POST':
+        return render_template("response.html",username=request.form['username'],method=request.method);
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
