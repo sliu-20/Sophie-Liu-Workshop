1c12c962699052b1e267c760a5ce6704d8825cc7
Sophie Liu <90879496+sliu-20@users.noreply.github.com>
2021-11-23 22:23:23 -0500

    Updated app.py with working code

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template, request, session, redirect
-import os
+# Sophie Liu, Ryan Wang
+# SoftDev
+# K19: A RESTful Journey Skyward
+# 2021-11-23
+
+from flask import Flask, render_template
+from json import loads
+import urllib
 
 app = Flask(__name__)
-app.secret_key = os.urandom(32)
 
-@app.route("/", methods=['GET', 'POST'])
-return render_template("main.html", pic="123")
+key = ""
+with open("key_nasa.txt") as file:
+    key = file.read()
+
+json = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=%s" % key).read()
+dict = loads(json)
+
+@app.route("/")
+def main():
+    return render_template("main.html", pic=dict['url'], ex=dict['explanation'])
+
+app.run()
