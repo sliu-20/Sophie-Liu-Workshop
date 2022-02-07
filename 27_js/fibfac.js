8059cb8f97905f1cadedfac07cf4f4f45f2c9249
Sophie Liu <sliu20@stuy.edu>
2022-02-06 23:02:37 -0500

    k27 work

--- 27_js/fibfac.js
+++ 27_js/fibfac.js
@@ -16,27 +16,19 @@ var fact = (n) => {
   else {
     return (fact(n - 1) * n);
   }
-};
+}
 
-// (define fact (int n)
-//   (lambda (n)
-//     (if (n = 0)
-//         1
-//         else {
-//
-//         }
-//          (fact(- n 1)) n)
-//         )
-//     )
-//   )
-//
-// (define fib
-//   (lambda (n)
-//     (if (<= n 1)
-//         n
-//         (+ (fib(- n 1)) (fib(- n 2)))
-//         )
-//     )
-//   )
+// prints fact 5 directly into dev console; gives 120, fact works!
+console.log(fact(5));
 
-//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
+var fib = function(n) {
+  if (n <= 1){
+    return n;
+  }
+  else{
+    return fib(n-1) + fib(n-2);
+  }
+}
+
+// gives 5 in dev console, fib fxn works!
+console.log(fib(5));
---
+++ 27_js/fibfact.scm
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fibfact) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define fact
+  (lambda (n)
+    (if (= n 0)
+        1
+        (* (fact(- n 1)) n)
+        )
+    )
+  )
+
+(define fib
+  (lambda (n)
+    (if (<= n 1)
+        n
+        (+ (fib(- n 1)) (fib(- n 2)))
+        )
+    )
+  )
+
+(fact 5)
+(fib 5)
